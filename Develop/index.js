3bf325885b5a0f9cf093ff253bb676a4993ed1f9
abi <abimael.monarrez58@gmail.com>
2022-01-16 18:08:39 -0700

    create array of questions for user input

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,11 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["Enter your project title", "Write a description of your project", "Describe the installation instructions", 
+                   "Write information about your project´s usage", "list the contribution guidelines", "Describe information about how to test your project", 
+                   "Choose a license for your application", "Enter your GitHub username", "Enter your e-mail address"];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
